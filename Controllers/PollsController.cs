943de560e046f7092e39e157b8f3731bb9f3d6d7
Hemida <adhamhemida4@gmail.com>
2025-02-03 19:48:15 +0200

    Add Endpoint

--- Controllers/PollsController.cs
+++ Controllers/PollsController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SurveyBasket.Models;
 
 namespace SurveyBasket.Controllers;
 [Route("api/[controller]")]// /api/polls
 [ApiController]
 public class PollsController : ControllerBase
 {
+	private static List<Poll> _polls = new List<Poll>();
+	[HttpGet]
+	public IActionResult Get()
+	{
+		return Ok(_polls);
+	}
 }
