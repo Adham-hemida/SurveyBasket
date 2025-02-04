a51e4361e342223bc8ad0b90be976cd4ed0fc81a
Hemida <adhamhemida4@gmail.com>
2025-02-04 13:26:17 +0200

    Make Interface IpollService and make class that implement from it

--- Controllers/PollsController.cs
+++ Controllers/PollsController.cs
@@ -4,10 +4,6 @@
 [ApiController]
 public class PollsController : ControllerBase
 {
-	private static List<Poll> _polls = new List<Poll> {
-		new Poll{Id=1,Title="First Poll",Description="This is the first poll"},
-		new Poll{Id=2,Title="Second Poll",Description="This is the second poll"},
-	};
 	[HttpGet]
 	[Route("getall")]
 	public IActionResult Get()
---
+++ Services/PollService.cs
@@ -0,0 +1,19 @@
+﻿namespace SurveyBasket.Services;
+
+public class PollService : IPollService
+{
+	private static List<Poll> _polls = new List<Poll> {
+		new Poll{Id=1,Title="First Poll",Description="This is the first poll"},
+		new Poll{Id=2,Title="Second Poll",Description="This is the second poll"},
+	};
+
+	public IEnumerable<Poll> GetAll()
+	{
+		return _polls;
+	}
+
+	public Poll? GetById(int id)
+	{
+		return _polls.SingleOrDefault(p => p.Id == id);
+	}
+}
