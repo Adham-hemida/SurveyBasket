329401a3b5a05c0e972a6232c37ab1f85018b38e
Hemida <adhamhemida4@gmail.com>
2025-02-05 22:35:09 +0200

    Used Manual Mapping Using Extension Methods

---
+++ Contracts/PollResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SurveyBasket.Contracts;
+
+public class PollResponse
+{
+
+	public int Id { get; set; }
+	public string Title { get; set; } = string.Empty;
+	public string Description { get; set; } = string.Empty;
+}
--- Services/IPollService.cs
+++ Services/IPollService.cs
@@ -1,4 +1,5 @@
-﻿namespace SurveyBasket.Services;
+﻿
+namespace SurveyBasket.Services;
 
 public interface IPollService
 {
