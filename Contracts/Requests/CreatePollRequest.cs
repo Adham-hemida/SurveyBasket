9820ebf2708dfe37c2475f1f706c8cff53940f65
Hemida <adhamhemida4@gmail.com>
2025-02-06 12:04:57 +0200

    correct code

--- Contracts/Requests/CreatePollRequest.cs
+++ Contracts/Requests/CreatePollRequest.cs
@@ -4,13 +4,15 @@ public class CreatePollRequest
 {
 	public string Title { get; set; } = string.Empty;
 	public string Description { get; set; } = string.Empty;
-}
+
 					
     public static implicit operator Poll(CreatePollRequest request)
 	{
-		return new( )
+		return new()
 		{
 			Title = request.Title,
 			Description = request.Description
 		};
-	}
+	} 
+
+}
