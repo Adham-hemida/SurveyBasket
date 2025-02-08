52ca22f2d7037aedc33fec55f33e6eef11a62f53
Hemida <adhamhemida4@gmail.com>
2025-02-08 16:59:51 +0200

    Mapster if the attribute did not same as the property name, we can use Map method to map the property

--- Contracts/Responses/PollResponse.cs
+++ Contracts/Responses/PollResponse.cs
@@ -1,3 +1,3 @@
 ﻿namespace SurveyBasket.Contracts.Responses;
-public record PollResponse(int Id ,string Title ,string Notes );
+public record PollResponse(int Id ,string Title ,string Description );
 	
--- Mapping/MappingConfiguarations.cs
+++ Mapping/MappingConfiguarations.cs
@@ -4,7 +4,10 @@ public class MappingConfiguarations : IRegister
 {
 	public void Register(TypeAdapterConfig config)
 	{
-		 config.NewConfig<Poll, PollResponse>() // Poll to pollresponse 
+		//if the attribute did not same as the property name, we can use Map method to map the property
+		config.NewConfig<Poll, PollResponse>() // Poll to pollresponse 
 			.Map(dest => dest.Notes, src => src.Description);
+		config.NewConfig<CreatePollRequest, Poll>() // CreatePollRequest to Poll
+			.Map(dest => dest.Description, src => src.Notes);
 	}
 }
