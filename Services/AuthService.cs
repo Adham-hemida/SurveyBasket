11613b91e4ee4cccec78157c10b11748aaaa54f9
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-02-21 19:34:54 +0200

    Add Get Refresh token Method

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -22,6 +22,7 @@ public class AuthService(UserManager<ApplicationUser> userManager,IJwtProvider j
 			return null;
 		//generate token
 		var (token, expiresIn) = _jwtProvider.GenerateJwtToken(user);
+		//generate refreshToken
 		var refreshToken = GenerateRefreshToken();
 		var refreshTokenExpiration = DateTime.UtcNow.AddDays(_refreshTokenExpirationDays);
 
@@ -36,8 +37,46 @@ public class AuthService(UserManager<ApplicationUser> userManager,IJwtProvider j
 
 
 	}
+
+
+	public async Task<AuthResponse?> GetRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default)
+	{
+		//I need to validate the token and refreshToken
+		var userId = _jwtProvider.ValidateToken(token);
+		if (userId is null)
+			return null;
+		//if i reach here, the token is valid and userId is not null and i got it
+		//get the user from the database
+		var user = await _userManager.FindByIdAsync(userId);
+		if (user is null)
+			return null;
+		//check if the refreshToken is valid(that mean The refreshToken i send is true) or not and get it from the database and check if it is active or not 
+		var userRefreshToken = user.RefreshTokens.SingleOrDefault(x => x.Token == refreshToken && x.IsActive);
+		if (userRefreshToken is null)
+			return null;
+		//if i reach here, the refreshToken is valid and i got it from the database 
+		//I revoked it as i need user to use it one time and need to generate new one
+		userRefreshToken.RevokedOn = DateTime.UtcNow;
+
+		// i need to generate new token and refreshToken
+		//generate new  token
+		var (newToken, expiresIn) = _jwtProvider.GenerateJwtToken(user);
+		//generate new refreshToken
+		var newRefreshToken = GenerateRefreshToken();
+		var newRefreshTokenExpiration = DateTime.UtcNow.AddDays(_refreshTokenExpirationDays);
+
+		//save refresh token in Database
+		user.RefreshTokens.Add(new RefreshToken
+		{
+			Token = newRefreshToken,
+			ExpiresOn = newRefreshTokenExpiration
+		});
+		await _userManager.UpdateAsync(user);
+		return new AuthResponse(user.Id, user.Email, user.FirstName, user.LastName, newToken, expiresIn,newRefreshToken, newRefreshTokenExpiration);
+
+	}
 	private static string GenerateRefreshToken()
 	{
-		return Convert.ToBase64String(RandomNumberGenerator.GetBytes(32));	
+		return Convert.ToBase64String(RandomNumberGenerator.GetBytes(32));
 	}
 }
