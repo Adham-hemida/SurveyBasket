3c353e3c6331e0137a63a79468e4f64b2ac6ac97
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-02-22 12:27:09 +0200

    Added AuditColumns To Polls Table

--- Entities/Poll.cs
+++ Entities/Poll.cs
@@ -1,12 +1,13 @@
 ﻿namespace SurveyBasket.Entities;
 
-public class Poll()
+public class Poll(): AuditableEntity
 {
 	
 	public int Id { get; set; }
-	public string Title { get; set; }
-	public string Summary { get; set; }
+	public string Title { get; set; }=string.Empty;
+	public string Summary { get; set; }= string.Empty;	
 	public bool IsPublished { get; set; }
 	public DateOnly StartsAt { get; set; }
 	public DateOnly EndsAt { get; set; }
+
 }
