3755b2013e1cf44f594a302ae8f2428819d97741
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-02-25 14:37:33 +0200

    using Problems

--- Services/IAuthService.cs
+++ Services/IAuthService.cs
@@ -3,7 +3,7 @@ namespace SurveyBasket.Services;
 
 public interface IAuthService
 {
-	//Task<Result<AuthResponse>> GetTokenAsync(string email, string password, CancellationToken cancellationToken = default);
+	
 	Task<Result<AuthResponse>> GetTokenAsync(LoginRequest loginRequest, CancellationToken cancellationToken = default);
 	Task<Result<AuthResponse>> GetRefreshTokenAsync(RefreshTokenRequest refreshTokenRequest, CancellationToken cancellationToken = default);
 	Task<Result> RevokeRefreshTokenAsync(RefreshTokenRequest refreshTokenRequest, CancellationToken cancellationToken = default);
