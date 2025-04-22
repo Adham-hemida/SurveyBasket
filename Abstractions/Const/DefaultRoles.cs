e5727c7e0a23a3cda5a4367b69d7edd4fc603ebc
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-04-22 18:14:38 +0200

    Added default data and seeding data for AspNetRoles Table

---
+++ Abstractions/Const/DefaultRoles.cs
@@ -0,0 +1,12 @@
+﻿namespace SurveyBasket.Abstractions.Const;
+
+public static class DefaultRoles
+{
+	public const string Admin = nameof(Admin);
+	public const string AdminRoleId = "01965d90-71b8-72b4-a99f-de3bf84d13de";
+	public const string AdminRoleConcurrencyStamp = "01965d90-71b8-72b4-a99f-de3c75cefcf7";
+
+	public const string Member = nameof(Member);
+	public const string MemberRoleId = "01965d90-71b8-72b4-a99f-de3d048430e9";
+	public const string MemberRoleConcurrencyStamp = "01965d90-71b8-72b4-a99f-de3e7ed6cc0c";
+}
