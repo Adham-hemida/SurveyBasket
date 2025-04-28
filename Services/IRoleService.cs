de746a7972552f02091171a007e4163b3e41c690
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-04-28 17:18:01 +0300

    Added Get All Roles Endpoint

---
+++ Services/IRoleService.cs
@@ -0,0 +1,8 @@
+﻿using SurveyBasket.Contracts.Roles;
+
+namespace SurveyBasket.Services;
+
+public interface IRoleService
+{
+	Task<IEnumerable<RoleResponse>> GetAllAsync(bool? includeDisabled = false, CancellationToken cancellationToken = default);
+}
---
+++ Services/RoleService.cs
@@ -0,0 +1,19 @@
+﻿using SurveyBasket.Contracts.Roles;
+
+namespace SurveyBasket.Services;
+
+public class RoleService(RoleManager<ApplicationRole> roleManager):IRoleService
+{
+	private readonly RoleManager<ApplicationRole> _roleManager = roleManager;
+
+	public async Task<IEnumerable<RoleResponse>> GetAllAsync(bool? includeDisabled=false,CancellationToken cancellationToken=default)
+	{
+		return await _roleManager.Roles
+			.Where(x=>!x.IsDefault&&( !x.IsDeleted || includeDisabled == true))
+			.Select(x => new RoleResponse
+			(x.Id,
+			x.Name!,
+			x.IsDeleted))
+			.ToListAsync(cancellationToken);
+	}
+}
