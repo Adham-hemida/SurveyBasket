5b2f22da98cc997d7a53d24a70f179bcb82312c2
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-05-03 20:27:15 +0300

    Added  Get All Users Endpoint

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyBasket.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController(IUserService userService) : ControllerBase
+{
+	private readonly IUserService _userService = userService;
+
+	[HttpGet("")]
+	[HasPermission(Permissions.GetUsers)]
+	public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+	{
+	  return Ok(await _userService.GetAllAsync(cancellationToken));
+	}
+}
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,12 +1,48 @@
 ﻿
 using Microsoft.AspNetCore.Identity;
 using SurveyBasket.Contracts.Users;
+using System.Data;
+using System.Linq;
 
 namespace SurveyBasket.Services;
 
-public class UserService(UserManager<ApplicationUser> userManager) : IUserService
+public class UserService(UserManager<ApplicationUser> userManager,
+	ApplicationDbContext context) : IUserService
 {
 	private readonly UserManager<ApplicationUser> _userManager = userManager;
+	private readonly ApplicationDbContext _context = context;
+
+
+	public async Task<IEnumerable<UserResponse>> GetAllAsync(CancellationToken cancellationToken = default)
+	{
+		return await (from u in _context.Users
+					  join ur in _context.UserRoles
+					  on u.Id equals ur.UserId
+					  join r in _context.Roles
+					  on ur.RoleId equals r.Id into roles
+					  where !roles.Any(x => x.Name == DefaultRoles.Member)
+					  select new
+					  {
+						  u.Id,
+						  u.FirstName,
+						  u.LastName,
+						  u.Email,
+						  u.IsDisabled,
+						  Roles = roles.Select(x => x.Name!).ToList()
+					  }
+				)
+				.GroupBy(u => new { u.Id, u.FirstName, u.LastName, u.Email, u.IsDisabled })
+				.Select(u => new UserResponse
+				(
+					u.Key.Id,
+					u.Key.FirstName,
+					u.Key.LastName,
+					u.Key.Email,
+					u.Key.IsDisabled,
+					u.SelectMany(x => x.Roles)
+				))
+			   .ToListAsync(cancellationToken);
+	}
 
 	public async Task<Result<UserProfileResponse>> GetProfileInfoAsync(string userId)
 	{
@@ -40,7 +76,8 @@ public class UserService(UserManager<ApplicationUser> userManager) : IUserServic
 			return Result.Success();
 
 		var error = result.Errors.First();
-		return Result.Failure(new Error(error.Code,error.Description,StatusCodes.Status400BadRequest));
+		return Result.Failure(new Error(error.Code, error.Description, StatusCodes.Status400BadRequest));
 	}
+
 
 }
