9d8b7c60e563f800ba4d28674b72c501e02cf0a4
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-06-20 20:41:53 +0300

    Remove all comments from ResultService

--- Services/ResultService.cs
+++ Services/ResultService.cs
@@ -10,20 +10,6 @@ public class ResultService(ApplicationDbContext context,IUnitOfWork unitOfWork) 
 
 	public async Task<Result<PollVotesResponse>> GetPollVotesAsync(int pollId, CancellationToken cancellationToken = default)
 	{
-		//var pollVotes = await _context.Polls
-		//	.Where(x => x.Id == pollId)
-		//	.Select(x => new PollVotesResponse(
-		//		x.Title,
-		//		x.Votes.Select(v => new VoteResponse(
-		//			$"{v.User.FirstName} {v.User.LastName}",
-		//			v.SubmittedOn,
-		//			v.VoteAnswers.Select(a => new QuestionAnswerResponse(
-		//				a.Question.Content,
-		//				a.Answer.Content
-		//				))
-		//			))
-		//		))
-		//	.SingleOrDefaultAsync(cancellationToken);
 
 		var pollVotes = await _unitOfWork.Repository<Poll>()
 			.GetAsQueryable()
@@ -47,20 +33,12 @@ public class ResultService(ApplicationDbContext context,IUnitOfWork unitOfWork) 
 
 	public async Task<Result<IEnumerable<VotesPerDayResponse>>> GetVotesPerDayAsync(int pollId, CancellationToken cancellationToken = default)
 	{
-		//var pollIsExists = await _context.Polls.AnyAsync(x => x.Id == pollId, cancellationToken);
 		var pollIsExists = await _unitOfWork.Repository<Poll>()
 			.AnyAsync(x => x.Id == pollId, cancellationToken);
 
 		if (!pollIsExists)
 			return Result.Failure<IEnumerable<VotesPerDayResponse>>(PollErrors.PollNotFound);
 
-		//var VotesPerDay = await _context.Votes
-		//	.Where(x => x.PollId == pollId)
-		//	.GroupBy(x => new { voteDate = DateOnly.FromDateTime(x.SubmittedOn) })
-		//	.Select(g => new VotesPerDayResponse(
-		//		g.Key.voteDate,
-		//		g.Count()))
-		//	.ToListAsync(cancellationToken);
 
 		var VotesPerDay = await _unitOfWork.Repository<Vote>()
 			.GetAsQueryable()
@@ -75,24 +53,12 @@ public class ResultService(ApplicationDbContext context,IUnitOfWork unitOfWork) 
 
 	public async Task<Result<IEnumerable<VotesPerQuestionResponse>>> GetVotesPerQuestionAsync(int pollId, CancellationToken cancellationToken = default)
 	{
-		//var pollIsExists = await _context.Polls.AnyAsync(x => x.Id == pollId, cancellationToken);
 		var pollIsExists = await _unitOfWork.Repository<Poll>()
 			.AnyAsync(x => x.Id == pollId, cancellationToken);
 
 		if (!pollIsExists)
 			return Result.Failure<IEnumerable<VotesPerQuestionResponse>>(PollErrors.PollNotFound);
 
-		//var VotesPerQuestion = await _context.VoteAnswers
-		//	.Where(x => x.Vote.PollId == pollId)
-		//	.Select(x => new VotesPerQuestionResponse(
-		//		x.Question.Content,
-		//		x.Question.Votes
-		//		   .GroupBy(g => new { AnswerId = g.Answer.Id, AnswerContent = g.Answer.Content })
-		//		   .Select(g => new VotesPerAnswerResponse(
-		//			  g.Key.AnswerContent,
-		//			  g.Count()
-		//			))
-		//		)).ToListAsync(cancellationToken);
 
 		var VotesPerQuestion = await _unitOfWork.Repository<VoteAnswer>()
 			.GetAsQueryable()
